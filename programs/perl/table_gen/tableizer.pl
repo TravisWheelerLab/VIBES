086634db9b0aeec48818c285f9c3dc78e0a92cd2
Conner <connercopeland01@gmail.com>
2018-06-06 13:34:29 -0600

    Created test version of tableizer that prints strings rather than running terminal commands. Also added a .txt file that I use to keep track of commands I've run, to help preserve it in case I manage to destroy it somehow

--- programs/perl/table_gen/tableizer.pl
+++ programs/perl/table_gen/tableizer.pl
@@ -1,12 +1,30 @@
 #!/usr/bin/env perl
 #Takes in a path to a genome directoy, a path to a file containing reference
-#.hmm files, and a path to an output directory. Returns a .dfam table for each
+#.hmm, and a path to an output directory. Returns a .dfam table for each
 #genome in the directory.
-#Arg 0: genome directory
 
 use strict;
 use warnings;
 
-my $dir = $ARGV[0];
+my $genomeDir = $ARGV[0];
+my $referencePath = $ARGV[1];
+my $tableDir = $ARGV[2];
+
 #we assume every entry in the directory is a genome we're interested in
-my @genomes = glob "$dir/*";
+my @genomes = glob "$genomeDir/*";
+
+#Extract file name from path, then run nhmmscan and dfamscan
+foreach my $genome (@genomes) {
+    my @splitLine = split('/', $genome);
+    my $fileName;
+
+    if ($splitLine[-1] =~ /(.+?)\./) {
+        $fileName = $1;
+    }
+
+    my $tablePath = "$tableDir/$fileName" . "_viral.dfam";
+    my $scannedPath = "$tableDir/$fileName" . "_viral_scanned.dfam";
+
+    print "nhmmscan --dfamtblout $tablePath $referencePath $genome\n";
+    print "perl dfamscan.pl --dfam_infile  $tablePath --dfam_outfile $scannedPath";
+}
