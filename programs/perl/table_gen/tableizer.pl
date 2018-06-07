61ae274b55fdf06a144f45f677d5411d75927469
Conner <connercopeland01@gmail.com>
2018-06-07 15:45:05 -0600

    Significantly changed tableizer.pl to work on cluster (the structure was substantially different from what I thought it would be). Now the first argument taken in is a job number assigned by the cluster, followed by a path to a genome dir, a path to a reference .hmm, a path to the table output file, and a suffix to append to the table names to help ensure uniqueness. Currently, the job on the server is not successfully completing, but I'm not sure whether that's due to a problem in this script or the job submission script

--- programs/perl/table_gen/tableizer.pl
+++ programs/perl/table_gen/tableizer.pl
@@ -3,37 +3,35 @@
 #.hmm, a path to an output directory, and a suffix to append to the file name.
 #Returns a .dfam table for each genome in the directory.
 
+#Credit to Kaitlin Carey for help in understanding how to run things on the cluster!
+
 use strict;
 use warnings;
 
-my $genomeDir = $ARGV[0];
-my $referencePath = $ARGV[1];
-my $tableDir = $ARGV[2];
-my $suffix = $ARGV[3];
-
-unless ($suffix) {
-    die "Please enter a valid suffix argument to help distinguish which reference"
-        . " strains are being used. Order of arguments is: Path to genome dir,"
-        . " path to reference strain file, path to output dir, suffix argument\n";
-}
+#Genome number is
+my $genomeNumber = $ARGV[0];
+my $genomeDir = $ARGV[1];
+my $referencePath = $ARGV[2];
+my $tableDir = $ARGV[3];
+my $suffix = $ARGV[4];
 
 #we assume every entry in the directory is a genome we're interested in
 my @genomes = glob "$genomeDir/*";
 
 #Extract file name from path, then run nhmmscan and dfamscan
-foreach my $genome (@genomes) {
-    my @splitLine = split('/', $genome);
-    my $fileName;
+my $genome = $genomes[$genomeNumber];
+my @splitLine = split('/', $genome);
+my $fileName;
 
-    if ($splitLine[-1] =~ /(.+?)\./) {
-        $fileName = $1;
-    }
-
-    my $tablePath = "$tableDir/$fileName" . "_$suffix.dfam";
-    my $scannedPath = "$tableDir/$fileName" . "_$suffix" . "_scanned.dfam";
-
-    print "\nBeginning nhmmscan on $genome\n";
-    `nhmmscan --dfamtblout $tablePath $referencePath $genome`;
-    print "\nBeginnning dfamscan.pl on $tablePath\n";
-    `perl dfamscan.pl --dfam_infile  $tablePath --dfam_outfile $scannedPath`;
+if ($splitLine[-1] =~ /(.+?)\./) {
+    $fileName = $1;
 }
+else {
+    die "Unable to extract file name from path: $genome\n";
+}
+
+my $tablePath = "$tableDir/$fileName" . "_$suffix.dfam";
+my $scannedPath = "$tableDir/$fileName" . "_$suffix" . "_scanned.dfam";
+
+`nhmmscan --dfamtblout $tablePath $referencePath $genome`;
+`perl dfamscan.pl --dfam_infile  $tablePath --dfam_outfile $scannedPath`;
