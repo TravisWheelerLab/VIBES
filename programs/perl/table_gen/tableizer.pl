3c4f5ab55b20f58cc23f8b155b3d6a7592a3c2fb
Conner <connercopeland01@gmail.com>
2018-06-11 13:18:46 -0600

    Version of tableizer.pl that sticks print statements and backticks into subroutines to make code more manageable

--- programs/perl/table_gen/tableizer.pl
+++ programs/perl/table_gen/tableizer.pl
@@ -1,6 +1,7 @@
 #!/usr/bin/env perl
 #Takes in a path to a genome directoy, a path to a file containing reference
-#.hmm, a path to an output directory, and a suffix to append to the file name.
+#.hmm, a path to an output directory, a suffix to append to the file name,
+#and a boolean that sets the value of verbose.
 #Returns a .dfam table for each genome in the directory.
 
 #Credit to Kaitlin Carey for help in understanding how to run things on the cluster!
@@ -14,6 +15,7 @@ my $genomeDir = $ARGV[1];
 my $referencePath = $ARGV[2];
 my $tableDir = $ARGV[3];
 my $suffix = $ARGV[4];
+my $verbose = $ARGV[5];
 
 #we assume every entry in the directory is a genome we're interested in
 my @genomes = glob "$genomeDir/*";
@@ -33,5 +35,39 @@ else {
 my $tablePath = "$tableDir/$fileName" . "_$suffix.dfam";
 my $scannedPath = "$tableDir/$fileName" . "_$suffix" . "_scanned.dfam";
 
-`nhmmscan --dfamtblout $tablePath $referencePath $genome`;
-`perl dfamscan.pl --dfam_infile  $tablePath --dfam_outfile $scannedPath`;
+do_cmd("nhmmscan --dfamtblout $tablePath $referencePath $genome");
+do_cmd("perl dfamscan.pl --dfam_infile  $tablePath --dfam_outfile $scannedPath");
+
+sub be_verbose {
+    our $genomeNumber;
+    our $genomeDir;
+    our $referencePath;
+    our $tableDir;
+    our $suffix;
+    our $genome;
+    our $tablePath;
+    our $scannedPath;
+    my $path = `pwd`;
+
+    print "Number: $genomeNumber\n";
+    print STDERR "Number: $genomeNumber\n";
+    print "GenDir: $genomeDir\n";
+    print "Ref: $referencePath\n";
+    print "Table dir: $tableDir\n";
+    print "Suffix: $suffix\n";
+    print "Genome: $genome\n";
+    print "Table Path: $tablePath\n";
+    print "Scanned table path: $scannedPath\n";
+    print "Current directory: $path";
+}
+
+sub do_cmd {
+    our $verbose;
+    my $cmd = $_[0];
+
+    if ($verbose > 0) {
+        print "$cmd\n";
+    }
+
+    return `$cmd`;
+}
