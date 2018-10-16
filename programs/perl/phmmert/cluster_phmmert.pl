115bda199b7a31c8616e1af732e6f944c3810d8a
Conner <connercopeland01@gmail.com>
2018-10-16 11:30:49 -0600

    Added help page, force option, and checking before overwriting files to cluster_phmmert.pl. Changed command line argument names to be shorter or more obvious as well

--- programs/perl/phmmert/cluster_phmmert.pl
+++ programs/perl/phmmert/cluster_phmmert.pl
@@ -12,6 +12,7 @@ my $referenceHMMPath = '';
 my $outputDir  = '';
 my $verbose = 0;
 my $help = 0;
+my $force = 0;
 
 #other variables
 my $outputPath = '';
@@ -19,14 +20,17 @@ my $genome = '';
 my $elapsed = '';
 
 GetOptions (
-    "referencehmm=s"    => \$referenceHMMPath,
-    "outputdir=s"       => \$outputDir,
-    "jobnumber=i"       => \$$genomeNumber,
-    "genomes=s"         => \$genomeDir,
-    "verbose"           => \$verbose,
-    "help"              => \$help
+    "hmm=s"         => \$referenceHMMPath,
+    "outputdir=s"   => \$outputDir,
+    "jobnumber=i"   => \$genomeNumber,
+    "genomedir=s"   => \$genomeDir,
+    "verbose"       => \$verbose,
+    "help"          => \$help,
+    "force"         => \$force
     )
 or die("Unknown argument, try --help\n");
+
+run_phmmer();
 
 sub run_phmmer {
     #put all files in directory into an array
@@ -47,6 +51,7 @@ sub run_phmmer {
 
     $outputPath = "$outputDir/$fileName.tbl";
 
+    checkFile($outputPath);
     do_cmd("phmmert --tblout $outputPath $referenceHMMPath $genome");
 }
 
@@ -60,8 +65,38 @@ sub do_cmd {
     return `$cmd`;
 }
 
-sub help {
+sub help { print "
+# cluster_phmmert.pl: runs phmmert with --tblout on genomes
 
+Usage: perl cluster_phmmert [options] --hmm [path] --genomedir [path]
+    --jobnumber [int] --outputdir [path]
+
+Basic Options:
+    --help: Displays this help page
+    --verbose: Prints commands run and information intended for debugging
+    --force: With this selected, any files with the same name as an output file
+        in the output directory will be automatically overwritten.
+
+Input Options:
+    --genomedir: Path to directory populated with genomes to run phmmert against
+    --jobnumber: Int assigned by server cluster, which specifies a genome to use
+    --hmm: Path to reference HMM to use as phmmert query
+
+Output Options:
+    --outputdir: Path to directory to be filled with output phmmert tables"
+}
+
+# Check if a file already exists. If yes, and --force hasn't been specified by the user,
+# print an error to a log file and STDERR, then stop the program. If --force has
+# been specified, then overwrite the file
+sub checkFile {
+   my $file = $_[0];
+
+   if (-f $file && !$force) {
+        open(my $errorlog, '>>', "cluster_phmmert_errors.txt") or die "Could not open file '$file' $!";
+        print $errorlog "$file already exists! To overwrite any files that already exist, rerun with --force option.\n\n";
+        die "$file already exists! To overwrite any files that already exist, rerun with --force option.\n";
+  }
 }
 
 sub be_verbose {
