48f9af38fb1ee21a0ae22d1032e78a95f5ba6cf5
Conner <connercopeland01@gmail.com>
2018-11-28 13:41:17 -0700

    Initial working version of the chart visualization program

---
+++ programs/python/visualize/visualize_nucleotide_counts.py
@@ -0,0 +1,6 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+#read in all files in total counts dir
+#for each file, populate an array in which each index corresponds to a line
+#plot as line graph
