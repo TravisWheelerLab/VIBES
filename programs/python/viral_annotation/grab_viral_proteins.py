407b0ff12445e63a2ad7dc37de71e9082cccb18f
Conner <connercopeland01@gmail.com>
2019-05-06 16:42:56 -0600

    Finished initial version of grap_viral_proteins.py. It seems to be working, but has 10 more entries than are found by egrep piped to wc

--- programs/python/viral_annotation/grab_viral_proteins.py
+++ programs/python/viral_annotation/grab_viral_proteins.py
@@ -7,17 +7,46 @@ import sys
 # credit to Viktor Kerkez in: https://stackoverflow.com/questions/18160078/how-do-you-write-tests-for-the-argparse-portion-of-a-python-module
 def parseArgs(sysArgs):
     parser = argparse.ArgumentParser()
-    parser.add_argument("input.fasta", help="Path to unfiltered .fasta file containing both viral and non-viral proteins")
-    parser.add_argument("output.fasta", help="Path to output filtered .fasta file containing viral protein sequence")
+    parser.add_argument("input", help="Path to unfiltered .fasta file containing both viral and non-viral proteins")
+    parser.add_argument("output", help="Path to output filtered .fasta file containing viral protein sequence")
     parser.add_argument("--force", help="If output files already exist, overwrite them", action="store_true")
 
-
-# Grab all entries from input .fasta that have 'virus' or 'phage' in their headers. Discard any that don't.
-def filterEntries(inputFasta):
-
-    matchResult = re.match(r'(virus|phage)', headerLine, re.IGNORECASE)
+    return parser.parse_args()
 
 
+# Grab all entries from input .fasta that have 'virus' or 'phage' in their headers and save them in output .fasta file. Discard any that don't.
+# Print how many matches were found
+def filterEntries(inputFasta, outputFasta):
+    # Boolean that we'll use to check if we want an entry's sequence data
+    matchingEntry = False
+    savedEntries = 0
+
+    with open(outputFasta, "w") as outputFileHandle:
+        with open(inputFasta, "r") as inputFileHandle:
+            for line in inputFileHandle:
+                # .fasta format has a header line denoted by >, followed by a line or lines of whatever sequence. If the file starts with >, it's
+                # a header line
+                if re.match(r'>', line):
+                    # if the header line contains virus or phage (regardless of upper or lower case), we want to save it to output. Set boolean to
+                    # true. Otherwise, set boolean to false and ignore
+                    if re.search(r'(virus|phage)', line, re.IGNORECASE):
+                        matchingEntry = True
+                        outputFileHandle.write(line)
+                        savedEntries += 1
+
+                    else:
+                        matchingEntry = False
+
+                # else, it's a sequence line. If the entry matches, we want to save it; otherwise, we ignore it
+                else:
+                    if matchingEntry:
+                        outputFileHandle.write(line)
+
+    print("%d entries contained virus or phage in their headers" % savedEntries)
+
+
+# check that we're running the program, rather than just grabbing its functions (perhaps for testing in another program)
 if __name__ == "__main__":
-    print("Hi")
     args = parseArgs(sys.argv[1:])
+
+    filterEntries(args.input, args.output)
