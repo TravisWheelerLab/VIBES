760d5870807f404f9a3d15b0971d9d1ec8314f49
Conner <connercopeland01@gmail.com>
2019-05-07 14:39:30 -0600

    Rewrote grab_viral_proteins to be faster and simpler. Most of the discrepency between egrep's matches and my program's have been addressed: Now it's finding exactly 1 less match than egrep

--- programs/python/viral_annotation/grab_viral_proteins.py
+++ programs/python/viral_annotation/grab_viral_proteins.py
@@ -7,46 +7,53 @@ import sys
 # credit to Viktor Kerkez in: https://stackoverflow.com/questions/18160078/how-do-you-write-tests-for-the-argparse-portion-of-a-python-module
 def parseArgs(sysArgs):
     parser = argparse.ArgumentParser()
-    parser.add_argument("input", help="Path to unfiltered .fasta file containing both viral and non-viral proteins")
+    parser.add_argument("input", help="Path to unfiltered .fasta file containing both viral and non-viral protein sequence")
     parser.add_argument("output", help="Path to output filtered .fasta file containing viral protein sequence")
-    parser.add_argument("--force", help="If output files already exist, overwrite them", action="store_true")
+    parser.add_argument("-f", "--force", help="If output files already exist, overwrite them", action="store_true")
+    parser.add_argument("-v", "--verbose", help="Print number of matching entries found", action="store_true")
 
     return parser.parse_args()
 
 
 # Grab all entries from input .fasta that have 'virus' or 'phage' in their headers and save them in output .fasta file. Discard any that don't.
 # Print how many matches were found
-def filterEntries(inputFasta, outputFasta):
-    # Boolean that we'll use to check if we want an entry's sequence data
-    matchingEntry = False
+def filterEntries(inputFasta, outputFasta, force, verbose):
+    # Counter to keep track of, and ultimate report the number of, matching entries found
     savedEntries = 0
 
-    with open(outputFasta, "w") as outputFileHandle:
-        with open(inputFasta, "r") as inputFileHandle:
-            for line in inputFileHandle:
-                # .fasta format has a header line denoted by >, followed by a line or lines of whatever sequence. If the file starts with >, it's
-                # a header line
-                if re.match(r'>', line):
-                    # if the header line contains virus or phage (regardless of upper or lower case), we want to save it to output. Set boolean to
-                    # true. Otherwise, set boolean to false and ignore
-                    if re.search(r'(virus|phage)', line, re.IGNORECASE):
-                        matchingEntry = True
-                        outputFileHandle.write(line)
+    # Since this regex statement will be used many times, we compile it ahead of time to save a little time. This regex statement
+    # looks for any occurances of 'virus' or 'phage', ignoring case
+    virusRegex = re.compile(r'(virus|phage)', re.I)
+
+    # This variable stores either 'w' or 'x'. 'w' tells it to overwrite the output if it already exists (--force set),
+    # 'x' instructs open() to only open the output file if it doesn't already exist (--force not set),
+    # credit for conditional assignment syntax to https://stackoverflow.com/questions/394809/does-python-have-a-ternary-conditional-operator
+    openPermission = 'w' if force else 'x'
+
+
+    try:
+        with open(outputFasta, openPermission) as outputFileHandle:
+            with open(inputFasta, "r") as inputFileHandle:
+                inputData = inputFileHandle.read()
+                # use regex to split all entries in the file along the > character, which denotes the beginning of a .fasta entry
+                entriesList = re.split(r'>', inputData)
+
+                for entry in entriesList:
+                    # if the header line contains virus or phage (regardless of upper or lower case), we want to save it to output
+                    if re.search(virusRegex, entry):
+                        outputFileHandle.write(">%s" % entry)
                         savedEntries += 1
 
-                    else:
-                        matchingEntry = False
+        if verbose:
+            print("%d entries contained virus or phage in their headers" % savedEntries)
 
-                # else, it's a sequence line. If the entry matches, we want to save it; otherwise, we ignore it
-                else:
-                    if matchingEntry:
-                        outputFileHandle.write(line)
-
-    print("%d entries contained virus or phage in their headers" % savedEntries)
+    # catch case where --force isn't set and file already exists:
+    except FileExistsError:
+        print("Output file %s already exists! To overwrite it and its contents, run with --force" % outputFasta)
 
 
 # check that we're running the program, rather than just grabbing its functions (perhaps for testing in another program)
 if __name__ == "__main__":
     args = parseArgs(sys.argv[1:])
 
-    filterEntries(args.input, args.output)
+    filterEntries(args.input, args.output, args.force, args.verbose)
