79e8456ea3e8228e09ecc0256a212d53ec1b4536
Conner <connercopeland01@gmail.com>
2019-07-16 16:31:11 -0600

    Rewrote generate_domtbls.py to accept path to hmm database from user, rather than requiring changes to code. Rewrote some --help info

--- programs/python/visualize/generate_domtbls.py
+++ programs/python/visualize/generate_domtbls.py
@@ -5,7 +5,7 @@ import re
 import sys
 
 
-def DomTblGen(prophageName, inputDir, outputDir):
+def DomTblGen(prophageName, inputDir, hmmPath, outputDir):
 
     outputFile = "%s/%s.domtbl" % (outputDir, prophageName)
     inputFile = "%s/%s.fasta" % (inputDir, prophageName)
@@ -17,24 +17,24 @@ def DomTblGen(prophageName, inputDir, outputDir):
 
     else:
         # run hmmscant to create .domtbl of prophage
-        command = "hmmscant --domtblout %s ../../../sequence_files/hmm/TIGRFAM.HMM %s" % (outputFile, inputFile)
+        command = "hmmscant --domtblout %s %s %s" % (outputFile, hmmPath, inputFile)
         subprocess.run(command.split())
 
 # credit to Viktor Kerkez in: https://stackoverflow.com/questions/18160078/how-do-you-write-tests-for-the-argparse-portion-of-a-python-module
 def parseArgs(sysArgs):
     parser = argparse.ArgumentParser()
-    parser.add_argument("inputDir", help="Path to directory of .fasta files. Expected format is [sequenceName].fasta")
-    parser.add_argument("outputDir", help="Path to directory to store output .domtbl files")
+    parser.add_argument("fasta_dir", help="Path to directory of .fasta files. Expected format is [sequenceName].fasta")
+    parser.add_argument("hmm_db", help="Path to HMM database to run against .fasta files")
+    parser.add_argument("output_dir", help="Path to directory to store output .domtbl files")
     parser.add_argument("--force", help="If output files already exist, overwrite them", action="store_true")
     return parser.parse_args()
 
 
 if __name__ == "__main__":
     args = parseArgs(sys.argv[1:])
-    inputDir = args.inputDir
-
-    inputDir = args.inputDir
-    outputDir = args.outputDir
+    inputDir = args.fasta_dir
+    hmmPath = args.hmm_db
+    outputDir = args.output_dir
     force = args.force
 
     # credit to pycruft in https://stackoverflow.com/questions/3207219/how-do-i-list-all-files-of-a-directory for code for grabbing file paths
@@ -49,4 +49,4 @@ if __name__ == "__main__":
         # extract name from file path
         prophageName = regMatch.group(1)
 
-        DomTblGen(prophageName, inputDir, outputDir)
+        DomTblGen(prophageName, inputDir, hmmPath, outputDir)
