bc87144276ca4e83f40ab18a9b73025a3c3ef23e
Conner <connercopeland01@gmail.com>
2021-08-12 15:25:17 -0600

    Initial version of .tsv writer func

--- programs/python/table_parser_py/table_parser.py
+++ programs/python/table_parser_py/table_parser.py
@@ -24,27 +24,50 @@ class ViralSeq:
         self.verbose = verbose
         self.full_cutoff = full_cutoff
         self.bac_genome_len = self.get_bac_len()
-        self.flanking_att_site = self.detect_flanking_atts()
+        self. is_flanked, self.flanking_att_site = self.detect_flanking_atts()
 
-    def get_vir_seq_len(self) -> int:
-        return abs(self.gn_st - self.gn_end) + 1
+    def get_seq_len(self) -> int:
+        return abs(self.bac_st - self.bac_end) + 1
 
     def get_bac_len(self) -> int:
         seqstat_results = do_cmd(f"esl-seqstat {self.bac_genome_path}", self.verbose)
         for line in seqstat_results.split("\n"):
             if "Total # residues" in line:
                 line_list = line.split()
-                return line_list[3]
+                return int(line_list[3])
 
     def get_percent_complete(self) -> float:
-        return float(self.len) / self.ref_vir_len
+        return float(self.get_seq_len()) / self.ref_vir_len
 
     def is_full_len(self) -> bool:
-        return (self.ref_vir_len - self.len) <= self.full_cutoff
+        return (self.ref_vir_len - self.get_seq_len()) <= self.full_cutoff
 
-    def detect_flanking_atts(self):
+    def detect_flanking_atts(self) -> Tuple[bool, List[str]]:
         # TODO: use simple smith-waterman alignment to search for off-diagonal hits at the ends of viral genome
-        pass
+        return False, ["",""]
+
+    def to_tsv_line(self) -> str:
+        return f"{self.name}\t{self.evalue}\t{self.is_full_len()}\t{self.ref_vir_st}\t{self.ref_vir_end}\t" \
+               f"{self.ref_vir_len}\t{self.is_flanked}\t{path.basename(self.bac_genome_path)}\t{self.bac_st}\t" \
+               f"{self.bac_end}\t{self.bac_genome_len}\t{self.strand}\n"
+
+def populate_tsv(tsv: TextIO, seq_list: List[ViralSeq], verbose: bool, force: bool):
+    # write header line first
+    tsv.write("Name\tE-Value\tIs Full Length Insertion\tMatch Start Position on Viral Genome\tMatch End Position on "
+              "Viral Genome\tViral Genome Length\tFlanking Att Sites\tBacterial Genome Name\tMatch Start Position on "
+              "Bacterial Genome\tMatch End Position on Bacterial Genome\tBacterial Genome Length\tStrand\n")
+    for viral_seq in seq_list:
+        tsv.write(viral_seq.to_tsv_line())
+
+
+def populate_tsv_from_path(tsv_path: str, seq_list: List[ViralSeq], verbose: bool, force: bool):
+    if path.isfile(tsv_path) and not force:
+        raise FileExistsError(
+            f"Output file {tsv_path} already exists- either move or delete this file or enable --force")
+    else:
+        with open(tsv_path, "w") as tsv:
+            populate_tsv(tsv, seq_list, verbose, force)
+
 
 def parse_table(dfam_file: TextIO, genome_path: str, verbose) -> List[ViralSeq]:
     seq_list = []
@@ -54,13 +77,13 @@ def parse_table(dfam_file: TextIO, genome_path: str, verbose) -> List[ViralSeq]:
         else:
             line_list = line.split()
             name = line_list[0]
-            evalue = line_list[4]
-            hmm_st = line_list[6]
-            hmm_en = line_list[7]
+            evalue = float(line_list[4])
+            hmm_st = int(line_list[6])
+            hmm_en = int(line_list[7])
             strand = line_list[8]
-            ali_st = line_list[9]
-            ali_en = line_list[10]
-            ref_vir_len = line_list[13]
+            ali_st = int(line_list[9])
+            ali_en = int(line_list[10])
+            ref_vir_len = int(line_list[13])
 
             seq_list.append(ViralSeq(name, evalue, ali_st, ali_en, genome_path, hmm_st, hmm_en, ref_vir_len, strand, verbose))
 
@@ -78,7 +101,8 @@ def parse_args(sys_args: list) -> argparse.Namespace:
     parser.add_argument("dfam_path", type=str, help="Path to input .dfam file")
     parser.add_argument("genome_path", type=str, help="Path to input genome in .fasta format")
     parser.add_argument("output_tsv_path", type=str, help="Path to output .tsv file")
-    parser.add_argument("output_json_path", type=str, help="Path to output .json file containing information on nucleotide occurrence counts")
+    parser.add_argument("output_json_path", type=str, help="Path to output .json file containing information on "
+                                                           "nucleotide occurrence counts")
     parser.add_argument("--max_evalue", type=float, default=default_eval, help=f"Maximum allowable sequence e-value (must be >= 0). Default is {default_eval}")
     parser.add_argument("--verbose", action="store_true", help="Print additional information useful for debugging")
     parser.add_argument("--force", action="store_true", help="If output file already exists, overwrite it")
@@ -86,7 +110,7 @@ def parse_args(sys_args: list) -> argparse.Namespace:
     return parser.parse_args()
 
 
-def do_cmd(cmd: str, verbose: bool) -> subprocess.CompletedProcess:
+def do_cmd(cmd: str, verbose: bool) -> str:
     if verbose:
         print(f"Running command: {cmd}")
 
@@ -108,7 +132,7 @@ def _main():
         raise ValueError("--max_evalue must be used with an argument greater than or equal to 0")
 
     viral_seqs = parse_table_from_path(dfam_path, genome_path, verbose)
-    print(viral_seqs[0].bac_genome_len)
+    populate_tsv_from_path(tsv_path, viral_seqs, verbose, force)
 
 
 if __name__ == "__main__":
